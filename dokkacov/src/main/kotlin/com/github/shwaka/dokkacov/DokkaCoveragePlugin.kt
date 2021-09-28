f2a5ac762aa003e1d596a00f23098b02b5610c60
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-09-28 19:05:26 +0900

    use nio.file.Path

--- dokkacov/src/main/kotlin/com/github/shwaka/dokkacov/DokkaCoveragePlugin.kt
+++ dokkacov/src/main/kotlin/com/github/shwaka/dokkacov/DokkaCoveragePlugin.kt
@@ -7,8 +7,12 @@ import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import org.jsoup.select.Elements
 import java.io.File
+import java.nio.file.Path
+import java.nio.file.Paths
 
 class DokkaCoveragePlugin : Plugin<Project> {
+    private val dokkaHtmlDirectory: Path = Paths.get("build/dokka/html")
+
     override fun apply(project: Project) {
         project.tasks.register("hello") {
             println("Hello from DokkaCoveragePlugin!")
@@ -20,7 +24,7 @@ class DokkaCoveragePlugin : Plugin<Project> {
     }
 
     private fun showCoverage() {
-        val indexHtml: File = File("build/dokka/html/index.html")
+        val indexHtml: File = this.dokkaHtmlDirectory.resolve("index.html").toFile()
         val doc: Document = Jsoup.parse(indexHtml, "UTF-8")
         val rows: Elements = doc.select("div.table-row")
         for (row: Element in rows) {
